18bea7ff9fb8a6e83ba2797c7a9b12e00e7a328e
Maxwell Coelho <maxwell.coelho@globalhitss.com.br>
2024-03-26 10:31:34 -0300

    Ultimos ajustes de build para lançar o mvp

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,29 +1,5 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+import { environment as defaults } from './environment.defaults';
 
 export const environment = {
-  production: false,
-  middlewareEndpoint: 'http://localhost:3001/api',
-  rolesAction: 'roles',
-  customersAction: 'customers',
-  categoriesAction: 'cars/categories',
-  brandsAction: 'cars/brands',
-  modelsAction: 'cars/models',
-  filterModelsAction: 'cars/models/filter',
-  versionsAction: 'cars/versions',
-  filterVersionsAction: 'cars/versions/filter',
-  opinionAction: 'opinion',
-  bestBrandsAction: 'best/brands',
-  bestModelsAction: 'best/models',
-  jstSecret: '-M@x-caRro$*_+'
+  ...defaults,
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
