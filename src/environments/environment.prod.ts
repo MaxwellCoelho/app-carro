98b6428110fb219c8ec7aa88171e72c52b74abb1
Maxwell Coelho <maxwellfcoelho@gmail.com>
2024-11-08 11:33:20 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import { environment as defaults } from './environment.defaults';
 export const environment = {
   ...defaults,
   production: true,
-  middlewareEndpoint: 'http://app-carro-api.railway.internal:3001/api',
+  middlewareEndpoint: 'https://krro.up.railway.app/api',
 };
