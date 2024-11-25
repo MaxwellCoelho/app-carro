cdb19c540ed308004d4488a241ca53335bd99e21
Maxwell Coelho <maxwell.coelho@globalhitss.com.br>
2024-11-25 16:45:59 -0300

    Continuando piloto com afilados

--- src/app/components/ads/affliated/affiliatedAdsList.ts
+++ src/app/components/ads/affliated/affiliatedAdsList.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 export const AFFILIATED_ADS_LIST = [
   {
     url: 'https://amzn.to/3Oqqt8u',
@@ -16,6 +17,45 @@ export const AFFILIATED_ADS_LIST = [
     images: [
       'https://m.media-amazon.com/images/I/71zj-+xJrbL._AC_SX679_.jpg',
       'https://m.media-amazon.com/images/I/81YrErlN1IL._AC_SX679_.jpg'
+    ],
+    keywords: []
+  },
+  {
+    url: 'https://amzn.to/4hXAZ4G',
+    title: 'Luva de Microfibra Tramontina',
+    description: 'A microfibra presente na luva com dupla camada, torna ela uma peça macia, perfeita para limpar superfícies delicadas sem riscar e soltar fiapos.',
+    images: [
+      'https://m.media-amazon.com/images/I/71ltY2d0zLL._AC_SX522_.jpg',
+      'https://m.media-amazon.com/images/I/816jnqXdAiL._AC_SX522_.jpg'
+    ],
+    keywords: []
+  },
+  {
+    url: 'https://amzn.to/3ZldtHD',
+    title: 'Proauto Limpa e Hidrata Couro 150 g',
+    description: 'Limpa e hidrata couro. Protege e dá brilho. Hidratante para borracha, couro, vinil e plástico.',
+    images: [
+      'https://m.media-amazon.com/images/I/71BeOWSqqtL._AC_SX522_.jpg',
+      'https://m.media-amazon.com/images/I/61GBecQGQiL._AC_SX522_.jpg'
+    ],
+    keywords: []
+  },
+  {
+    url: 'https://amzn.to/4fD64cm',
+    title: '3M Auto Elimina Riscos Para Carros, Alta performance, 120 gr',
+    description: 'Eficiente na remoção de micro-riscos superficiais como unhas, chaves e riscos de galhos. Fácil Aplicação. Deixa um ótimo acabamento no veículo.',
+    images: [
+      'https://m.media-amazon.com/images/I/71gdcNZRwQL._AC_SX522_.jpg',
+      'https://m.media-amazon.com/images/I/615JONt00EL._AC_SX522_.jpg'
+    ],
+    keywords: []
+  },
+  {
+    url: 'https://amzn.to/3ANIUkx',
+    title: 'Aroma Sachê Folha Quebec - Night Forest',
+    description: 'O ambiente, seja ele fechado ou pequeno, fica perfumado durante 45 dias. É indicado para veículos, guarda-roupas, armários, e Qualquer cômodos do seu lar.',
+    images: [
+      'https://m.media-amazon.com/images/I/51ymjTDeF6L._AC_SX522_.jpg'
     ],
     keywords: []
   },
